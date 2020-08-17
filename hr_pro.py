d8b76c7b9ffc4a64feea421743cbcf150e51ee88
atheen aleesa <atheen@atheens-iMac.local>
2020-08-17 11:30:02 +0300

    fixed hr-pro

--- hr_pro.py
+++ hr_pro.py
@@ -10,19 +10,24 @@ class Employee:
        self.employment_year = employment_year
    def get_working_years(self):
        return datetime.today().year-int(self.employment_year)
+   def __str__(self):
+        return "Name: %s, Age: %s, Salary: %s, Working Years: %d" % (self.name, self.age, self.salary, self.get_working_years())
 
 class Manager(Employee):
     #Manager class here
     def __init__(self,name,age,salary,employment_year,bonus_percentage):
-        Employee.__init__(self,name,age,salary,employment_year)
+        super().__init__(name,age,salary,employment_year)
         self.bonus_percentage = bonus_percentage
     def get_bonus(self):
         return float(self.bonus_percentage)*float(self.salary)
+    def __str__(self):
+         return "Name: %s, Age: %s, Salary: %s, Working Years: %d, bonus: %d" % (self.name, self.age, self.salary, self.get_working_years(),self.get_bonus())
+
 
 def main():
 	# main code here
-    managers = [Manager("sammy",52,4600,2005,.5)]
-    employees = [Employee("Ahmed",40,1500,2010)]
+    managers = []
+    employees = []
     print("Welcome to HR Pro 2019")
     print("Options:")
     print("\t1. Show Employees\n\t2. Show Managers\n\t3. Add An Employee\n\t4. Add A Manager\n\t5. Exit\n")
@@ -32,12 +37,12 @@ def main():
         if choice == "1":
             print("Employees\n")
             for e in employees:
-                print("Name: %s, Age: %s, Salary: %s, Working Years: %s"%(e.name,e.age,e.salary,e.get_working_years()))
-        if choice == "2":
+                print(e)
+        elif choice == "2":
             print("Managers\n")
             for m in managers:
-                print("Name: %s, Age: %s, Salary: %s, Working Years: %s, Bonus: %.6f"%(m.name,m.age,m.salary,m.get_working_years(),m.get_bonus()))
-        if choice == "3":
+                print(m)
+        elif choice == "3":
             name = input("Name: ")
             age = input("Age: ")
             salary = input("Salary: ")
@@ -45,7 +50,7 @@ def main():
             employee = Employee(name,age,salary,emp_year)
             employees.append(employee)
             print("Employee added succesfully")
-        if choice == "4":
+        elif choice == "4":
             name = input("Name: ")
             age = input("Age: ")
             salary = input("Salary: ")
@@ -54,6 +59,8 @@ def main():
             manager = Manager(name,age,salary,emp_year,bonus)
             managers.append(manager)
             print("Manager added succesfully")
+        else:
+            break
         print("-----------------\nOptions:")
         print("\t1. Show Employees\n\t2. Show Managers\n\t3. Add An Employee\n\t4. Add A Manager\n\t5. Exit\n")
         choice = input("What would you like to do? ")
